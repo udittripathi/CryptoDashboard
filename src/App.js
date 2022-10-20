eda2ea45773f1b05ce85bcdc533acf6312651e20
udittripathi <71835574+udittripathi@users.noreply.github.com>
2022-10-20 23:47:25 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from "react";
-import {Switch, Route,Link}  from 'react-router-dom';
-import { Layout, Typography, Space} from 'antd';
-import {Navbar} from './components';
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
-const App =() => {
-      return(
-        <div className="app">
-            <div className="navbar">
-                <Navbar/>
-            </div>
-            <div className="main">
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
+import './App.css';
 
-            </div>
-            <div className="footer">
-
-            </div>
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
         </div>
-      );
-}
+      </Layout>
+    </div>
+  </div>
+);
 
-export default App
+export default App;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'X-BingApis-SDK': 'true',
+  'X-RapidAPI-Key': '7f9a588c82msh4a074ba86e16fbfp1327efjsn58cd6db9294b',
+  'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_NEWS_API_URL }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
